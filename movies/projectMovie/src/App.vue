3958862a8ae5aeddf640a368f881093440f3ecfd
Oğuzhan Ahmet Demirci <152156995+oguzhandemirc@users.noreply.github.com>
2024-07-10 14:51:09 +0300

    router added

---
+++ movies/projectMovie/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div>
+  <header>
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ movies/projectMovie/src/views/AboutDetailView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about detail {{ route.params.id }} page</h1>
+  </div>
+</template>
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+console.log(route.params.id);
+</script>
+<style>
+
+</style>
