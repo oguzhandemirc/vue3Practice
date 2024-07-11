abf62060580f34bb7c881eb67a018a9b9b866388
Oğuzhan Ahmet Demirci <152156995+oguzhandemirc@users.noreply.github.com>
2024-07-11 13:45:38 +0300

    detailPage updated

--- movies/projectMovie/src/views/AboutDetailView.vue
+++ movies/projectMovie/src/views/AboutDetailView.vue
@@ -1,34 +1,65 @@
 <template>
-  <div class="about">
-    <h1
-      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-    >
-      {{ detailInfo.title }}
-      <!-- {{ detailInfo.original_language }}
-      {{ detailInfo.overview }}
-      {{ detailInfo.popularity }}
-      {{ detailInfo.poster_path }}
-      {{ detailInfo.release_date }}
-      {{ detailInfo.vote_count }}
-      {{ detailInfo.vote_average }} -->
-      <span class="text-blue-600 dark:text-blue-500"
-        >( {{ detailInfo.release_date }} )</span
-      >
-    </h1>
-    <img :src="`https://image.tmdb.org/t/p/w500/${detailInfo.poster_path}`" />
-    <h3>Film Puanı</h3>
-    <p>{{ detailInfo.vote_average }}</p>
-    <h3>Oylayan Kişi Sayısı</h3>
-    <p>{{ detailInfo.vote_count }}</p>
-    <h3>Özet</h3>
-    <p>{{ detailInfo.overview }}</p>
-    <h3>Popülerlik</h3>
-    <p>{{ detailInfo.popularity }}</p>
-  </div>
+  <body class="h-screen bg-gray-100 flex items-center justify-center">
+    <div class="about px-10 pt-10 bg-pink-100 pb-10 rounded-lg shadow-lg pl-40">
+      <div class="flex items-center">
+        <div>
+          <img
+            class="rounded-2xl"
+            :src="`https://image.tmdb.org/t/p/w500/${detailInfo.poster_path}`"
+          />
+        </div>
+        <div class="ml-20">
+          <h1
+            class="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+          >
+            {{ detailInfo.title }}
+            <span class="text-blue-600 dark:text-blue-500"
+              >( {{ detailInfo.release_date }} )</span
+            >
+          </h1>
+          <p class="uppercase">
+            {{ detailInfo.release_date }} - {{ detailInfo.original_language }}
+          </p>
+          <div
+            class="mt-5 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-3xl dark:text-white"
+          >
+            <h3>
+              <span class="text-red-600 text-5xl">{{ detailInfo.vote_average }}</span>
+              <span> Raiting </span>
+            </h3>
+          </div>
+          <div class="flex">
+            <h3 class="lg:text-2xl">
+              <span class="text-blue-600 dark:text-blue-500 lg:text-3xl font-extrabold">{{
+                detailInfo.vote_count
+              }}</span>
+              Kişi Tarafından Oylandı
+            </h3>
+          </div>
+          <div class="flex text-3xl font-extrabold">
+            <p>
+              {{ detailInfo.popularity }}
+              <span class="font-normal text-2xl">Kişi Tarafından İzledi</span>
+            </p>
+          </div>
+          <div>
+            <h2
+              class="mt-5 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-3xl dark:text-white"
+            >
+              Özet
+            </h2>
+            <div class="mt-4 font-extralight w-3/6">
+              <p>{{ detailInfo.overview }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script setup>
-import { onMounted, ref, watch } from "vue";
+import { onMounted, ref, watch, computed } from "vue";
 import { useRoute } from "vue-router";
 import axios from "axios";
 import { useCounterStore } from "@/stores/counter";
@@ -37,6 +68,7 @@ const route = useRoute();
 const store = useCounterStore();
 const localID = ref(route.params.id);
 const detailInfo = ref([]);
+const movieLang = detailInfo.original_language;
 
 const loadDetail = () => {
   store.dataMovie.forEach((element) => {
@@ -51,9 +83,7 @@ const loadDetail = () => {
 
 onMounted(() => {
   loadDetail();
-  console.log(store.dataMovie);
 });
-
 // watch(
 //   () => route.params.id,
 //   async () => {
