765aa6f5adb9cf6070dbf078a0f9e66117b57656
Oğuzhan Ahmet Demirci <152156995+oguzhandemirc@users.noreply.github.com>
2024-07-16 15:17:34 +0300

    pagination added

--- movies/projectMovie/src/components/homeComponents/movieList.vue
+++ movies/projectMovie/src/components/homeComponents/movieList.vue
@@ -62,10 +62,64 @@
         </li>
       </ul>
     </div>
+    <div class="flex items-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div class="flex flex-1 justify-start">
+        <p class="text-sm text-gray-700">
+          Showing
+          {{ " " }}
+          <span class="font-medium">{{ store.pageCount }}</span>
+          {{ " " }}
+          of
+          {{ " " }}
+          <span class="font-medium">{{ store.maxPage }}</span>
+        </p>
+      </div>
+      <div class="hidden sm:flex sm:flex-1 justify-end">
+        <ul class="flex isolate inline-flex -space-x-px rounded-md shadow-sm">
+          <li>
+            <button
+              class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <ChevronLeftIcon
+                @click="store.prevPage"
+                class="h-5 w-5"
+                aria-hidden="true"
+              />
+            </button>
+          </li>
+          <li class="pr-3 pagination" v-for="item in store.paginatedNumbers" :key="item">
+            <button
+              @click="
+                store.setPage(item);
+                console.log(item);
+              "
+              class="relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              :class="[
+                store.pageCount === item ? 'bg-indigo-600' : 'bg-white text-gray-900',
+              ]"
+            >
+              {{ item }}
+            </button>
+          </li>
+          <li>
+            <button
+              class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <ChevronRightIcon
+                class="h-5 w-5"
+                aria-hidden="true"
+                @click="store.nextPage"
+              />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
 import { useCounterStore } from "@/stores/counter.js";
 const store = useCounterStore();
 </script>
--- movies/projectMovie/src/stores/counter.js
+++ movies/projectMovie/src/stores/counter.js
@@ -1,30 +1,80 @@
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 import { defineStore } from "pinia";
 import axios from "axios";
 
 export const useCounterStore = defineStore("counter", () => {
   const dataMovie = ref([]);
-  const searchTerm = ref(""); // Arama terimi için doğru tip
+  const searchTerm = ref("");
+  const pageCount = ref(1);
+  const totalPages = ref(0);
+  const totalMovie = ref(0);
+  const range = ref(5);
+  const numbers = ref([]);
+  const maxPage = ref(500);
+
+  const setPage = (page) => {
+    pageCount.value = page;
+    scrollToTop();
+    updatePagination();
+  };
 
   const options = {
     method: "GET",
     url: "https://api.themoviedb.org/3/movie/popular",
-    params: { language: "en-US", page: "1" },
+    params: { language: "en-US", page: pageCount.value },
     headers: {
       accept: "application/json",
-      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGQ5NTg2ZjVkNjhlOTEzYzlhYzgxOWVjMTMzNzA3YSIsIm5iZiI6MTcyMDYxMzI1My45OTkyNjIsInN1YiI6IjY2OGU2MTA3NmNhMDRlZDhkYTBkNDFkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AGrSrAuTuMNMrDeimQqxF7n9YXnx45eaOVKqiC1F3gg",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGQ5NTg2ZjVkNjhlOTEzYzlhYzgxOWVjMTMzNzA3YSIsIm5iZiI6MTcyMDYxMzI1My45OTkyNjIsInN1YiI6IjY2OGU2MTA3NmNhMDRlZDhkYTBkNDFkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AGrSrAuTuMNMrDeimQqxF7n9YXnx45eaOVKqiC1F3gg",
     },
   };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const updatePagination = () => {
+    const startPage = Math.max(pageCount.value - range.value, 1);  
+    const endPage = Math.min(pageCount.value + range.value, totalPages.value);
+    numbers.value = [];
+    for (let i = startPage; i <= endPage; i++) {
+      numbers.value.push(i);
+    }
+  };
+
+  watch(pageCount, () => {
+    options.params.page = pageCount.value;
+    getReq();
+  });
 
   const getReq = async () => {
     await axios
       .request(options)
       .then(function (response) {
-        dataMovie.value = response.data.results.slice(0, 20);
+        dataMovie.value = response.data.results;
+        totalPages.value = response.data.total_pages;
+        totalMovie.value = response.data.total_results;
+        updatePagination();
       })
       .catch(function (error) {
         console.error(error);
       });
+  };
+
+  const nextPage = () => {
+    if (pageCount.value < totalPages.value) {
+      pageCount.value++;
+      scrollToTop();
+      updatePagination();
+    }
+  };
+
+  const prevPage = () => {
+    if (pageCount.value > 1) {
+      pageCount.value--;
+      scrollToTop();
+      updatePagination();
+    }
   };
 
   const filteredMovie = computed(() => {
@@ -33,9 +83,27 @@ export const useCounterStore = defineStore("counter", () => {
     );
   });
 
+  const paginatedNumbers = computed(() => {
+    return numbers.value.slice();
+  });
+
   onMounted(() => {
     getReq();
   });
 
-  return { dataMovie, searchTerm, filteredMovie, getReq };
+  return {
+    maxPage,
+    dataMovie,
+    searchTerm,
+    filteredMovie,
+    getReq,
+    totalPages,
+    totalMovie,
+    pageCount,
+    nextPage,
+    numbers,
+    setPage,
+    prevPage,
+    paginatedNumbers,
+  };
 });
